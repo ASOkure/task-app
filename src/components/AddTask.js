c8cb87f1477b61b47a2a5913cbacfc83e8954d5e
Akanimo Sam Okure <akan.okure@gmail.com>
2024-05-07 17:20:25 +0100

    implement edit and delete with local storage

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,14 +2,28 @@ export const AddTask = ({ tasklist, setTasklist, task, setTask }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const date = new Date();
-    const newTask = {
-      id: date.getTime(),
-      name: e.target.task.value,
-      time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
-    };
-    setTasklist([...tasklist, newTask]);
-    e.target.task.value = "";
+    if (task.id) {
+      const date = new Date();
+      const updatedTasklist = tasklist.map((todo) =>
+        todo.id === task.id
+          ? {
+              id: task.id,
+              name: task.name,
+              time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+            }
+          : todo
+      );
+      setTasklist(updatedTasklist);
+    } else {
+      const date = new Date();
+      const newTask = {
+        id: date.getTime(),
+        name: e.target.task.value,
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+      };
+      setTasklist([...tasklist, newTask]);
+      e.target.task.value = "";
+    }
   };
 
   return (
@@ -18,9 +32,11 @@ export const AddTask = ({ tasklist, setTasklist, task, setTask }) => {
         <input
           type="text"
           name="task"
+          value={task.name}
           autoComplete="off"
           placeholder="add task"
           maxLength="25"
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
         />
         <button type="submit">Add</button>
       </form>
